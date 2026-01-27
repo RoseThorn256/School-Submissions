f15f96b57a021b5b37fc722b0523af77cc7f7190
Rose Laidler <rosethorn256@gmail.com>
2026-01-27 12:08:32 -0700

    added variables.php

---
+++ variables/variables.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+$fName = "Rose";
+$lName = "Laidler";
+$address = "5476 S Katie ln";
+$phoneNumber = "(928) 732-0582";
+
+print "<p>My name is: <br>$fName $lName.<br><br>My (not real) address is: <br>$address<br><br>My (not real) phone number is: <br>$phoneNumber</p>"
+
+?>
+</body>
+</html>
