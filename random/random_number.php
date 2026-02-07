8cc455a04ea09c044805b10f5c764302e1955cf9
Rose Laidler <rosethorn256@gmail.com>
2026-02-07 10:17:44 -0700

    Add random_number.php

---
+++ random/random_number.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Random</title>
+</head>
+<body>
+<?php
+
+	$randomNum = rand(10, 100);
+	echo($randomNum);
+
+?>
+</body>
+</html>
