a79f04830dcd2c251bde8beaaba01ea805aab6d1
Rose Laidler <rosethorn256@gmail.com>
2026-02-07 11:36:01 -0700

    Add passhash.php

---
+++ encrypt/passhash.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Encrypt</title>
+</head>
+<body>
+	<?php
+
+		$password = "helloclass";
+		$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+		echo $hashed_password;
+
+	?>
+</body>
+</html>
