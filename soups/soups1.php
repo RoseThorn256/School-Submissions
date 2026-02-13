280ddef87c4d8275919bfad7033820db0135c2fe
Rose Laidler <rosethorn256@gmail.com>
2026-02-13 08:44:37 -0700

    Add soups1.php

---
+++ soups/soups1.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>No Soup for You!</title>
+</head>
+<body>
+	<h1>Mmm...soups</h1>
+	<?php
+		$soups = [
+		'Monday' => 'Clam Chowder',
+		'Tuesday' => 'White Chicken Chili',
+		'Wednesday' => 'Vegitarian'
+		];
+
+		print "<p>$soups</p>";
+
+		print_r($soups);
+	?>
+</body>
+</html>
