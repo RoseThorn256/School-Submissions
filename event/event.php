590f815aa45be9004d0bc46ee57e73e178547681
Rose Laidler <rosethorn256@gmail.com>
2026-02-14 10:13:17 -0700

    Add event.php

---
+++ event/event.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Add an Event</title>
+</head>
+<body>
+	<?php
+
+		print "<p>You want to add an event called <b>{$_POST['name']}</b> Which takes place on: <br>";
+
+		if (isset($_POST['days']) AND is_array($_POST['days'])) {
+
+			foreach ($_POST['days'] as $day) {
+				print "$day<br>\n";
+			}
+
+		} else {
+
+			print 'Please select at least one weekday for this event!';
+
+		}
+
+	print '</p>';
+
+	?>
+</body>
+</html>
