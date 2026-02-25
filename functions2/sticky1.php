b4f58202445b42323434903683c524a685cfcd1a
Rose Laidler <rosethorn256@gmail.com>
2026-02-25 09:35:52 -0700

    Add sticky1.php

---
+++ functions2/sticky1.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Sticky text inputs</title>
+</head>
+<body>
+<?php
+	function make_text_input($name, $label) {
+
+		echo '<p><label>' . $label . ': ';
+
+		echo '<input type="text" name="' . $name . '" size="20"';
+
+		if (isset($_POST[$name])) {
+			echo ' value="' . htmlspecialchars($_POST[$name]) . '"';
+		}
+
+		echo '></label></p>';
+	}
+
+	echo '<form action="" method="post">';
+
+	make_text_input('first_name', 'First Name');
+	make_text_input('last_name', 'Last Name');
+	make_text_input('email', 'Email Address');
+
+	echo '<input type="submit" name="submit" value="Register!"></form>';
+
+?>
+</body>
+</html>
