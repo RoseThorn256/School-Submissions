0a6a5c322a2a3828d2ae40ccd68396d40c127ab0
Rose Laidler <rosethorn256@gmail.com>
2026-03-01 09:40:27 -0700

    Add view_quote.php

---
+++ perms4/php/view_quote.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>View A Quotation</title>
+</head>
+<body>
+	<h1>Random Quotation</h1>
+	<?php
+
+		$data = file('../quotes.txt');
+
+		$n = count($data);
+
+		$rand = rand(0, ($n - 1));
+
+		echo '<p>' . trim($data[$rand]) . '</p>';
+
+	?>
+</body>
+</html>
